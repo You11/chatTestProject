59bb6dccf4d1620894f18d2053236ac46c8312ac
aa shulpin <aashulpin@gmail.com>
2018-05-13 20:24:40 +0500

    added table view, test data and other small things

---
+++ chatTestProject/chatTestProject/FriendTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FriendTableViewCell.swift
+//  chatTestProject
+//
+//  Created by Артем Шульпин on 13.05.2018.
+//  Copyright © 2018 Артем Шульпин. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    @IBOutlet weak var friendNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ chatTestProject/chatTestProject/User.swift
@@ -0,0 +1,19 @@
+//
+//  Friend.swift
+//  chatTestProject
+//
+//  Created by Артем Шульпин on 13.05.2018.
+//  Copyright © 2018 Артем Шульпин. All rights reserved.
+//
+
+import UIKit
+
+class User {
+    
+    //MARK: Properties
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
