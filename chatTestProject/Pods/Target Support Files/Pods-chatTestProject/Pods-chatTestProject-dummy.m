4a20e2b442914318a1cacf0eb383978359d9bdb0
aa shulpin <aashulpin@gmail.com>
2018-05-14 16:25:57 +0500

    some libraries added

---
+++ chatTestProject/Pods/Target Support Files/Pods-chatTestProject/Pods-chatTestProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatTestProject : NSObject
+@end
+@implementation PodsDummy_Pods_chatTestProject
+@end
---
+++ chatTestProject/chatTestProject/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chatTestProject
+//
+//  Created by Артем Шульпин on 13.05.2018.
+//  Copyright © 2018 Артем Шульпин. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
